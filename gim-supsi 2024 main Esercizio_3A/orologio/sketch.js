99d975a5f03f0dc998e1d0450f78360bc78b700e
MillenaarLuciana <167758986+MillenaarLuciana@users.noreply.github.com>
2024-05-13 14:26:19 +0200

    crea

---
+++ gim-supsi 2024 main Esercizio_3A/orologio/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+	background(255)
+
+	rect(width/2, height/2, 200, 50)
+
+
+}
